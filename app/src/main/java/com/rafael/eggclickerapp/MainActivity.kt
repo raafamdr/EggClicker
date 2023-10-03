c965aa0aac6a0565adb8906dbdfc31903a0bbe72
Rafael del Rio <rafaelgdelrio@gmail.com>
2023-10-03 20:08:34 -0300

    Adding Egg animation and counter

--- app/src/main/java/com/rafael/eggclickerapp/MainActivity.kt
+++ app/src/main/java/com/rafael/eggclickerapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.rafael.eggclickerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
+import com.rafael.eggclickerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var count = 0
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textCount.text = getString(R.string.egg_count, count)
+
+        binding.imgEgg.setOnClickListener {
+            it.animation = AnimationUtils.loadAnimation(this, R.anim.shake_animation)
+            count++
+            binding.textCount.text = getString(R.string.egg_count, count)
+            binding.textClick.visibility = View.GONE
+        }
     }
 }
